11bbf5c0ccce17ffb355e82b2e8f957477921071
VTADuong <vuanhduong95@gmail.com>
2026-05-08 11:45:30 +0700

    Add files via upload

---
+++ 10.4/10.4.js
@@ -0,0 +1,4 @@
+let firstNumber = Number (prompt('input first number'));
+let secondNumber = Number (prompt('input second number'));
+console.log('Total: ' + firstNumber + secondNumber);
+alert(+ firstNumber + secondNumber)

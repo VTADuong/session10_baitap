00b26ce375529a760772dbaeb36470a8a1579032
VTADuong <vuanhduong95@gmail.com>
2026-05-08 12:15:22 +0700

    Add files via upload

---
+++ 10.6/10.6.js
@@ -0,0 +1,5 @@
+let math = parseFloat(prompt('nhập điểm môn toán'));
+let physics = parseFloat(prompt('nhập điểm môn lý'));
+let chemistry = parseFloat(prompt('nhập điểm môn hóa'));
+let average = ((math + physics + chemistry) / 3)
+document.write(average)
